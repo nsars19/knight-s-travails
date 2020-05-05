9991a4ca77f9b4203356d5f64ca441b5374812b3
Nick <nsardonini@gmail.com>
2020-05-05 14:57:41 -0400

    add #coordinates

--- lib/square.rb
+++ lib/square.rb
@@ -1,8 +1,12 @@
-class Square
+class Square #or Vertex
   attr_reader :x_coord, :y_coord
   
   def initialize x_coordinate, y_coordinate
     @x_coord = x_coordinate
     @y_coord = y_coordinate
+  end
+
+  def coordinates
+    [@x_coord, @y_coord]
   end
 end

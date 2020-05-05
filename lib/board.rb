b696c2264ac4253a97dc6b0f0690bc3545b9b807
Nick <nsardonini@gmail.com>
2020-05-05 14:55:17 -0400

    add constructor, #populate_board, #get_coordinates

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,28 @@
+require_relative 'square'
+
 class Board
-  
+  attr_reader :board, :grid
+
+  def initialize
+    @board = populate_board
+    @grid = get_coordinates
+  end
+
+  def populate_board
+    squares = []
+    (1..8).each do |x_coord|
+      (1..8).each do |y_coord|
+        squares << Square.new(x_coord, y_coord)
+      end
+    end
+    squares
+  end
+
+  def get_coordinates
+    coordinates = []
+    @board.each do |square|
+      coordinates << square.coordinates
+    end
+    coordinates
+  end
 end
